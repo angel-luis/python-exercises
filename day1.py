898d20ef304ce7a5e331e0762676d89d242b4042
angel-luis <angel.luis@me.com>
2023-05-02 00:13:46 +0200

    Improve indentation for Github

--- day1.py
+++ day1.py
@@ -1,4 +1,7 @@
-""" Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line. """
+""" Write a program which will find all such numbers which are divisible
+by 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on
+a single line. """
 
 
 def exercise1():
@@ -10,7 +13,10 @@ def exercise1():
 print("\nExercise 1: ")
 exercise1()
 
-""" Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320 """
+""" Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program: 8
+Then, the output should be:40320 """
 
 
 def exercise2(num):
@@ -23,7 +29,10 @@ def exercise2(num):
 print("\n\nExercise 2: ")
 exercise2(8)
 
-""" With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
+""" With a given integral number n, write a program to generate
+a dictionary that contains (i, i x i) such that is an integral
+number between 1 and n (both included). and then the program should
+print the dictionary.Suppose the following input is supplied to the program: 8
 Then, the output should be:
 
 {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64} """
